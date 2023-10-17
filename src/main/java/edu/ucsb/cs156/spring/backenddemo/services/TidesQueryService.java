d41be47de6af8472b1e4466383cbb45410aaf86c
Wei Jung Huang <whuang602@csilvm-14.cs.ucsb.edu>
2023-10-17 16:41:32 -0700

    wh - implemented Tides service

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/TidesQueryService.java
@@ -25,9 +25,9 @@ public class TidesQueryService {
     }
 
     // Documentation for endpoint is at: https://api.tidesandcurrents.noaa.gov/api/prod/
-    public static final String ENDPOINT = "";
+    public static final String ENDPOINT = "https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?application=ucsb-cs156&begin_date=%s&end_date=%s&station=%s&product=predictions&datum=mllw&units=english&time_zone=lst_ldt&interval=hilo&format=json";;
 
     public String getJSON(String beginDate, String endDate, String station) throws HttpClientErrorException {
-        return "";
+        return String.format(ENDPOINT, beginDate, endDate, station);
     }
 }
