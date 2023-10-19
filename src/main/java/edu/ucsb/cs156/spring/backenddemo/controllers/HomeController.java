f7baf65a08108ff0485f644b1999b3a9128447c1
justinchung712 <justinchung712@gmail.com>
2023-10-19 14:35:57 -0700

    jc - customized home controller to include team member names and updated repo link

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/HomeController.java
@@ -43,12 +43,14 @@ public class HomeController {
         resultMap.put("greeting","Greetings from Spring Boot!");
 
         List<String> team = new ArrayList<String>();
-        team.add("Jing P.");
-        team.add("Christian S.");
-        team.add("Guy W.");
-        team.add("Phill C.");
+        team.add("Jeffrey C.");
+        team.add("Justin C.");
+        team.add("William H.");
+        team.add("Chris S.");
+        team.add("Vedant S.");
+        team.add("Zhenyu Y.");
         resultMap.put("team",team);
-        resultMap.put("repo","https://github.com/ucsb-cs156-f23/STARTER-team01");
+        resultMap.put("repo","https://github.com/ucsb-cs156-f23/team01-f23-6pm-2");
         resultMap.put("api-documentation", baseUrl + "swagger-ui/index.html");
         return mapper.writeValueAsString(resultMap);
     }
